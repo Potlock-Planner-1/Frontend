3b4026c8bf0b4cef46d71c8dd7e6dd9aac857df1
MMendes24 <mmbusiness1@protonmail.com>
2020-07-29 16:35:18 -0400

    Attempting minor fixes to the button in registration.

--- potluck/src/components/PotluckListMarsMVP.js
+++ potluck/src/components/PotluckListMarsMVP.js
@@ -1,38 +1,36 @@
-// import React, { useEffect, useState } from "react"
-// import Potluck from './Potluck';
-// import PotluckMars from '../components/PotluckMarsMVP'
-// import Axios from "axios";
+import React, { useEffect, useState } from "react"
+import PotluckMars from '../components/PotluckMarsMVP'
+import Axios from "axios";
 
 
 
-// const PotluckList = () => {
+const PotluckList = () => {
 
     // mars MVP shenanigans, sorry it's horrendously ugly but please leave it (or place it in a dummy file if it's just too annoying to look at)
     
-    // const [ potlucks, setPotlucks ] = useState([])
+    const [ potlucks, setPotlucks ] = useState([])
     
-    // useEffect(() => {
-    //     Axios.get('https://potluckplanner1.herokuapp.com/api/potlucks')
-    //     .then(res => {
-    //         console.log("We made it this far")
-    //         setPotlucks(res.data)
-    //     })
-    //     .catch( () => {
-    //         console.log('Snake eyes!')
-    //     })
-    // }, [])
+    useEffect(() => {
+        Axios.get('https://potluckplanner1.herokuapp.com/api/potlucks')
+        .then(res => {
+            console.log("We made it this far")
+            setPotlucks(res.data)
+        })
+        .catch( () => {
+            console.log('Snake eyes!')
+        })
+    }, [])
     
-        // return(
-            // <div className='container'>
-            {/* <Potluck />
+        return(
+            <div className='container'>
             {potlucks.map(potluck => {
                 return (
                 <PotluckMars key={potluck.id} potluck={potluck} />
                 )     
             })
-            } */}
-        // </div>
-    //     )
-    // };
+            } 
+        </div>
+        )
+    };
     
-    // export default PotluckList;
+    export default PotluckList;
