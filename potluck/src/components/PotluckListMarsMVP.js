987dd7e3fa16c2c0ce5913da2cd0bfd621d03467
MMendes24 <mmbusiness1@protonmail.com>
2020-07-29 15:44:52 -0400

    Put commented out code in a dummy file to hopefully prevent all future conflicts, i.e. option of last resort.

---
+++ potluck/src/components/PotluckListMarsMVP.js
@@ -0,0 +1,38 @@
+// import React, { useEffect, useState } from "react"
+// import Potluck from './Potluck';
+// import PotluckMars from '../components/PotluckMarsMVP'
+// import Axios from "axios";
+
+
+
+// const PotluckList = () => {
+
+    // mars MVP shenanigans, sorry it's horrendously ugly but please leave it (or place it in a dummy file if it's just too annoying to look at)
+    
+    // const [ potlucks, setPotlucks ] = useState([])
+    
+    // useEffect(() => {
+    //     Axios.get('https://potluckplanner1.herokuapp.com/api/potlucks')
+    //     .then(res => {
+    //         console.log("We made it this far")
+    //         setPotlucks(res.data)
+    //     })
+    //     .catch( () => {
+    //         console.log('Snake eyes!')
+    //     })
+    // }, [])
+    
+        // return(
+            // <div className='container'>
+            {/* <Potluck />
+            {potlucks.map(potluck => {
+                return (
+                <PotluckMars key={potluck.id} potluck={potluck} />
+                )     
+            })
+            } */}
+        // </div>
+    //     )
+    // };
+    
+    // export default PotluckList;
