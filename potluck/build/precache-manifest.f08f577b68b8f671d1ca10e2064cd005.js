97958d5a2b562230c9616e34a64eece24ec5ebc3
Parimala Vemula <parimala.thodupunoori@gmail.com>
2020-07-30 00:04:44 -0700

    Pushing build dir to deploy to netlify

---
+++ potluck/build/precache-manifest.f08f577b68b8f671d1ca10e2064cd005.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d229c1a8a52b0e327ea47c9f9468e14b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1df87886a41abe37deb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cd7b8d15b1c7f025a584",
+    "url": "/static/js/2.e9c59cc6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e9c59cc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1df87886a41abe37deb",
+    "url": "/static/js/main.b5b8de77.chunk.js"
+  },
+  {
+    "revision": "3b5ea85d9d743a77f842",
+    "url": "/static/js/runtime-main.280468dd.js"
+  }
+]);
